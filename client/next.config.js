1cee382978aa678c6b011fcd31d71568529bce6a
arvindkaphley <88549860+arvindkaphley@users.noreply.github.com>
2023-07-26 19:59:56 +0530

    Zego Authentication

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env:{
+    NEXT_PUBLIC_ZEGO_APP_ID:449721245,
+    NEXT_PUBLIC_ZEGO_SERVER_ID:"6c779e23b735524ef198a5e9766e8609",
+  },
   images:{
     domains: ["localhost"],
   },
